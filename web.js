db08e07098cc4da4821010078e8e28186535e4a9
Alexander Di Battista <alex.dibattista@gmail.com>
2013-07-08 02:59:00 +0000

    chagned web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
